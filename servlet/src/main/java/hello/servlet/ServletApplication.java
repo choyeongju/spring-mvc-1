72d30b659fbd38ec09a71bfe076516922bf7e138
choyeongju <jjudy1301@naver.com>
2024-08-11 21:05:41 +0900

    :sparkles: [feat] 서블릿

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
+@SpringBootApplication
+@ServletComponentScan // 서블릿 자동 등록
 public class ServletApplication {
 
 	public static void main(String[] args) {
